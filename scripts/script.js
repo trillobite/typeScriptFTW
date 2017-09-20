f620fb0ed453498dfc232990d3d33a21b672c907
trillobite <jparnell0@gmail.com>
2017-09-19 23:11:49 -0700

    working on the basics of nodejs

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+var hello;
+var printMsg = function (input) {
+    console.log(input); //will display to console the message.
+};
+hello = 'hello world';
+printMsg(hello);
---
+++ scripts/script.ts
@@ -0,0 +1,9 @@
+let hello: string;
+
+let printMsg = function(input) {
+    console.log(input); //will display to console the message.
+};
+
+hello = 'hello world';
+
+printMsg(hello);
